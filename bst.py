725ce6d98377f44f2914eaa5949eadcc52b158c2
Ryan <ryangilbert7926@gmail.com>
2019-01-02 01:52:25 -0500

    Initial commit :tada:

---
+++ bst.py
@@ -0,0 +1,82 @@
+from queue import Queue
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        if other == None:
+            return False
+        return (self.value == other.value and
+                self.left == other.left and
+                self.right == other.right)
+
+class BST:
+    def __init__(self):
+        self.root = None
+
+    def search(self, value):
+        if self.root == None:
+            return None
+
+        return self.__search(self.root, value)
+
+    def insert(self, value):
+        if self.root == None:
+            self.root = Node(value)
+            return self.root
+
+        if value == self.root.value:
+            return None
+
+        return self.__insert(self.root, value)
+
+    def traverse(self, mode):
+        if self.root == None:
+            return None
+
+        values = []
+        unvisited = Queue()
+        unvisited.put(self.root)
+
+        self.__level_order(values, unvisited)
+
+        return values
+
+    def __level_order(self, values, unvisited):
+        if unvisited.empty():
+            return
+
+        node = unvisited.get()
+        values.append(node.value)
+
+        if node.left:
+            unvisited.put(node.left)
+        if node.right:
+            unvisited.put(node.right)
+
+        self.__level_order(values, unvisited)
+
+    def __search(self, node, value):
+        if value == node.value:
+            return node
+        elif value < node.value:
+            return self.__search(node.left, value)
+        else:
+            return self.__search(node.right, value)
+
+    def __insert(self, node, value):
+        if value < node.value:
+            if node.left == None:
+                node.left = Node(value)
+                return node.left
+            else:
+                return self.__insert(node.left, value)
+        else:
+            if node.right == None:
+                node.right = Node(value)
+                return node.right
+            else:
+                return self.__insert(node.right, value)
---
+++ test_bst.py
@@ -0,0 +1,182 @@
+import unittest
+
+from bst import BST, Node
+
+class TestBSTBase(unittest.TestCase):
+    def setUp(self):
+        self.subject = BST()
+
+class TestBSTInsert(TestBSTBase):
+    def test_null(self):
+        result = self.subject.insert(100)
+
+        self.assertIsNotNone(self.subject.root)
+        self.assertEqual(self.subject.root, result)
+
+    def test_duplicate(self):
+        self.subject.insert(100)
+
+        result = self.subject.insert(100)
+
+        self.assertIsNone(result)
+
+    def test_left(self):
+        self.subject.insert(100)
+
+        result = self.subject.insert(99)
+
+        self.assertIsNotNone(self.subject.root.left)
+        self.assertEqual(self.subject.root.left, result)
+    
+    def test_right(self):
+        self.subject.insert(100)
+
+        result = self.subject.insert(101)
+
+        self.assertIsNotNone(self.subject.root.right)
+        self.assertEqual(self.subject.root.right, result)
+
+    def test_left_subtree(self):
+        self.subject.insert(100)
+        self.subject.insert(90)
+
+        result = self.subject.insert(80)
+
+        left_subtree = self.subject.root.left
+        self.assertIsNotNone(left_subtree.left)
+        self.assertEqual(left_subtree.left, result)
+
+    def test_right_subtree(self):
+        self.subject.insert(100)
+        self.subject.insert(110)
+
+        result = self.subject.insert(120)
+
+        right_subtree = self.subject.root.right
+        self.assertIsNotNone(right_subtree.right)
+        self.assertEqual(right_subtree.right, result)
+
+class TestBSTSearch(TestBSTBase):
+    def test_null(self):
+        result = self.subject.search(100)
+
+        self.assertIsNone(result)
+
+    def test_root(self):
+        self.subject.root = Node(100)
+
+        result = self.subject.search(100)
+
+        self.assertEqual(Node(100), result)
+
+    def test_left(self):
+        self.subject.root = Node(100)
+        self.subject.root.left = Node(50)
+
+        result = self.subject.search(50)
+
+        self.assertEqual(Node(50), result)
+
+    def test_right(self):
+        self.subject.root = Node(100)
+        self.subject.root.right = Node(150)
+
+        result = self.subject.search(150)
+
+        self.assertEqual(Node(150), result)
+    
+    def test_left_subtree(self):
+        self.subject.root = Node(100)
+        self.subject.root.left = Node(75)
+        self.subject.root.left.left = Node(50)
+
+        result = self.subject.search(50)
+
+        self.assertEqual(Node(50), result)
+
+    def test_right_subtree(self):
+        self.subject.root = Node(100)
+        self.subject.root.right = Node(125)
+        self.subject.root.right.right = Node(150)
+
+        result = self.subject.search(150)
+
+        self.assertEqual(Node(150), result)
+
+class TestBSTLevelOrderTraversal(TestBSTBase):
+    def test_null(self):
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertIsNone(result)
+
+    def test_root(self):
+        self.subject.root = Node(100)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([100], result)
+
+    def test_left(self):
+        self.subject.root = Node(80)
+        self.subject.root.left = Node(30)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([80, 30], result)
+
+    def test_right(self):
+        self.subject.root = Node(80)
+        self.subject.root.right = Node(100)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([80, 100], result)
+
+    def test_left_subtree(self):
+        self.subject.root = Node(5)
+        self.subject.root.left = Node(4)
+        self.subject.root.left.left = Node(3)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([5, 4, 3], result)
+
+    def test_right_subtree(self):
+        self.subject.root = Node(3)
+        self.subject.root.right = Node(4)
+        self.subject.root.right.right = Node(5)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([3, 4, 5], result)
+
+    def test_uneven_tree(self):
+        self.subject.root = Node(10)
+        self.subject.root.left = Node(8)
+        self.subject.root.right = Node(12)
+        self.subject.root.left.right = Node(5)
+        self.subject.root.right.left = Node(11)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual([10, 8, 12, 5, 11], result)
+
+    def test_medium_tree(self):
+        for i in range(-400, 400):
+            self.subject.insert(i)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual(len(result), 800)
+
+    @unittest.skip('This test case causes maximum recursion depth error.')
+    def test_large_tree(self):
+        for i in range(-100_000_000, 100_000_000):
+            self.subject.insert(i)
+
+        result = self.subject.traverse(mode='level_order')
+
+        self.assertEqual(len(result), 200_00_000)
+
+if __name__ == '__main__':
+    unittest.main()
