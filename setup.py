778193560298cae27c755b80f6e3e22ae3b3b974
Ryan <ryangilbert7926@gmail.com>
2019-01-05 20:28:49 -0500

    Add setup.py and related files to publish to PyPI

---
+++ setup.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
+import os
+import sys
+from shutil import rmtree
+
+from setuptools import find_packages, setup, Command
+
+NAME = 'bst'
+DESCRIPTION = 'A binary search tree implemented for learning purposes.'
+URL = 'https://github.com/rgilbert1/bst'
+EMAIL = 'ryangilbert7926@gmail.com'
+AUTHOR = 'Ryan Gilbert'
+REQUIRES_PYTHON = '>=3.6.0'
+VERSION = '0.1.0'
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+class UploadCommand(Command):
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(VERSION))
+        os.system('git push --tags')
+        
+        sys.exit()
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    py_modules=['bst'],
+    include_package_data=True,
+    license='MIT',
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+)
